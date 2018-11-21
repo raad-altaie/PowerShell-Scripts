4cd3bfc264d3f25fe615c209449542c6edcbda2c
Raad Altaie <raad.altaie@icloud.com>
2018-11-21 12:38:41 -0800

    Update CreateNewUserAD.ps1

--- CreateNewUserAD.ps1
+++ CreateNewUserAD.ps1
@@ -1,81 +1,89 @@
 # Script to Create New User in Active Dirctory through PowerShell 
 
-function CreateNewUser($FirstName, $LastName , $Title , $Manager, $UsertoCopyFrom ) {
-    #make username (all lowercase)
-    $Username = $FirstName.ToLower().ToCharArray()[0] + $LastName.ToLower()
+function CreateNewUser($FName, $LName, $UsertoCopyFrom ) {
+    try {
+        # Capitaize first and last names and git rid of spaces 
+        $FirstName = (Get-Culture).TextInfo.ToTitleCase($FName).trim()
+        $LastName = (Get-Culture).TextInfo.ToTitleCase($LName).trim()
 
-    # Get OU Path from the user that we are copying from 
-    $Path = ((Get-ADUser $UsertoCopyFrom -Properties *).DistinguishedName -split ",",2)[1]
+        # Make username (all lowercase)
+        $Username = $FirstName.ToLower().ToCharArray()[0] + $LastName.ToLower()
+
+        # Get OU Path from the user that we are copying from 
+        $Path = ((Get-ADUser $UsertoCopyFrom -Properties *).DistinguishedName -split ",", 2)[1]
 
 
 
-    $Attributes = @{
+        $Attributes = @{
 
-        Enabled               = $true
+            Enabled               = $true
 
-        ChangePasswordAtLogon = $true
+            ChangePasswordAtLogon = $true
  
-        # userlogon name (locally)
-        UserPrincipalName     = $Username + '@CompanyName.local'
+            # userlogon name
+            UserPrincipalName     = $Username + '@yourcompany.local'
  
-        #username
-        Name                  = $Username
+            #username
+            Name                  = $Username
  
-        # First Name
-        GivenName             = $FirstName
+            # First Name
+            GivenName             = $FirstName
 
  
-        # Last Name
-        Surname               = $LastName
+            # Last Name
+            Surname               = $LastName
  
-        #Email
-        EmailAddress           = $Username + '@companyname.com'  
+            #Email
+            EmailAddress          = $Username + '@yourcompany.com'  
 
-        #login Script 
-        ScriptPath            = 'script.bat'
+            ScriptPath            = 'YourBatScript.bat'
  
-        # Disply Nmae 
-        DisplayName           = $FirstName + ' ' + $LastName
+            # Disply Nmae 
+            DisplayName           = $FirstName + ' ' + $LastName
  
-        # Description
-        Description           = ''
+            # Description
+            Description           = ''
  
-        # Office
-        Office                = ''
+            # Office
+            Office                = ''
  
-        # Company
-        Company               = 'Company'
+            # Company
+            Company               = 'yourcompany'
  
-        # Department
-        Department            = (Get-ADUser $UsertoCopyFrom -Properties *).Department
+            # Department
+            Department            = (Get-ADUser $UsertoCopyFrom -Properties *).Department
  
-        # Title
-        Title                 = $Title
+            # Title
+            Title                 = (Get-ADUser $UsertoCopyFrom -Properties *).Title
 
-        # Manager
-        Manager               = $Manager
+            # Manager
+            Manager               = (Get-ADUser $UsertoCopyFrom -Properties *).Manager
  
-        AccountPassword       = "Password123" | ConvertTo-SecureString -AsPlainText -Force
+            AccountPassword       = "Password123" | ConvertTo-SecureString -AsPlainText -Force
+        }
+ 
+        #Create a user in AD on same OU as copyfromuser
+        New-ADUser @Attributes -Path $Path
+
+        #Change the Name to Display Name on CN in OU
+        Set-ADUser $Username -PassThru | Rename-ADObject -NewName $Attributes.DisplayName
+
+
+        #Copy the Groups from the Existing user in same Department 
+        $grouplist = (Get-ADUser $UsertoCopyFrom -Properties memberof).memberof
+        foreach ($group in $grouplist) {
+            Add-ADGroupMember $group -Members $Username
+        }  
+
+        write-host User: *$Username* created and the password is: *Password123*  
     }
- 
-    #Create a user in AD on same OU as the CopyFromUser
-    New-ADUser @Attributes -Path $Path
-
-    #Change the Name to Display Name on CN in OU
-    Set-ADUser $Username -PassThru | Rename-ADObject -NewName $Attributes.DisplayName
-
-
-    #Copy the Groups from the Existing user in same Department 
-    $grouplist = (Get-ADUser $UsertoCopyFrom -Properties memberof).memberof
-    foreach ($group in $grouplist) {
-        Add-ADGroupMember $group -Members $Username
-    }  
-
-    write-host User: $Username created and the password is: Password123  
+    catch {
+        write-host $Error[0]
+    }
 
 }
 
 # to create a user call the function e.g.
-CreateNewUser 'FirstName' 'LastName' 'Job Title' 'MangerUsername' 'UsertoCopyFrom'
+CreateNewUser 'FirstName' 'LastName' 'UsertoCopyFrom'
 
 
